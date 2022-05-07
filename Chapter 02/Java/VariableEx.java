7daefa6afd4c31073d8c6eb308296b2d74683311
Ajrdn <ajrdn1276@gmail.com>
2022-05-07 20:17:57 +0900

    VariableEx

---
+++ Chapter 02/Java/VariableEx.java
@@ -0,0 +1,19 @@
+package binary;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		
+		int num1;
+		num1 = 10;
+		System.out.println(num1);
+		
+		int level = 20;
+		System.out.println(level);
+		
+		long num2 = 12345678900L;
+		System.out.println(num2);
+		
+	}
+
+}
