f4f2fc9f6b7fc1acfb552a9874f2782e56629a26
Ajrdn <ajrdn1276@gmail.com>
2022-05-10 20:52:39 +0900

    ForExample2

---
+++ Chapter 04/loopexample/Java/ForExample2.java
@@ -0,0 +1,19 @@
+package loopexample;
+
+public class ForExample2 {
+
+	public static void main(String[] args) {
+		
+		int num = 1;
+		int sum = 0;
+		
+		for( ; num <= 10; ) {
+			sum += num;
+			num++;
+		}
+		
+		System.out.println(sum);
+		System.out.println(num);
+	}
+
+}
