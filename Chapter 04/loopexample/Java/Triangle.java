a1c99d5aa231c79a2461494b4236cc7fe11553fd
Ajrdn <ajrdn1276@gmail.com>
2022-05-11 18:47:34 +0900

    Triangle

---
+++ Chapter 04/loopexample/Java/Triangle.java
@@ -0,0 +1,27 @@
+package loopexample;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		
+		int min = 4, max = 4;
+		
+		for(int l = 1; l <= 4; l++) {
+			for(int i = 1; i <= min - 1; i++) {
+				System.out.print(" ");
+			}
+			
+			for(int i = min; i <= max; i++) {
+				System.out.print("*");
+			}
+			
+			for(int i = max + 1; i <= 7; i++ ) {
+				System.out.print(" ");
+			}
+			min--;
+			max++;
+			System.out.println();
+		}
+	}
+
+}
