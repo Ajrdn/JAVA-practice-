cf14ae3adfcb3b483b166a7cd669ce6cbd2890c3
Ajrdn <ajrdn1276@gmail.com>
2022-05-14 21:43:50 +0900

    Subject

---
+++ Chapter 05/reference/Java/Subject.java
@@ -0,0 +1,28 @@
+package reference;
+
+public class Subject {
+	
+	String subjectName;
+	int score;
+	
+	public Subject(String name) {
+		subjectName = name;
+	}
+	
+	public void setSubjectName(String name) {
+		subjectName = name;
+	}
+	
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
