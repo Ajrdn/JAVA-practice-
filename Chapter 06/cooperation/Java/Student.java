f873e8061439bab731af551ad4e11212d3888e30
Ajrdn <ajrdn1276@gmail.com>
2022-05-15 21:57:22 +0900

    Student

--- Chapter 06/cooperation/Java/Student.java
+++ Chapter 06/cooperation/Java/Student.java
@@ -27,7 +27,7 @@ public class Student {
 	}
 	
 	public void showInfo() {
-		System.out.println(studentName + "?? ?? ?? " + money + "? ???.");
+		System.out.println(studentName + "님의 남은 돈은 " + money + "원 입니다.");
 	}
 	
 }
