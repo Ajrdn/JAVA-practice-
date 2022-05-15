a1bf7ff2fc8321bb48d97a7c3b8121ea5c462026
Ajrdn <ajrdn1276@gmail.com>
2022-05-15 20:48:20 +0900

    Student

---
+++ Chapter 06/cooperation/Java/Student.java
@@ -0,0 +1,33 @@
+package cooperation;
+
+public class Student {
+	
+	String studentName;
+	int grade;
+	int money;
+	
+	public Student(String studentName, int money) {
+		this.studentName = studentName;
+		this.money = money;
+	}
+	
+	public void takeBus(Bus bus) {
+		bus.take();
+		money -= bus.charge;
+	}
+	
+	public void takeSubway(Subway subway) {
+		subway.take();
+		money -= subway.charge;
+	}
+	
+	public void takeTaxi(Taxi taxi) {
+		taxi.take();
+		money -= taxi.charge;
+	}
+	
+	public void showInfo() {
+		System.out.println(studentName + "´ÔÀÇ ³²Àº µ·Àº " + money + "¿ø ÀÔ´Ï´Ù.");
+	}
+	
+}
