ff3b8ff98a4c5ae417a3855d733fe68635d93b87
Ajrdn <ajrdn1276@gmail.com>
2022-05-20 13:51:59 +0900

    EnhancedForLoop

---
+++ Chapter 07/array/Java/EnhancedForLoop.java
@@ -0,0 +1,21 @@
+package array;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		
+		String[] strArr = {"Java", "Android", "C"};
+		
+		for(String i : strArr) {
+			System.out.println(i);
+		}
+		
+		int[] arr = {1, 2, 3, 4, 5};
+		
+		for(int i : arr) {
+			System.out.println(i);
+		}
+
+	}
+
+}
