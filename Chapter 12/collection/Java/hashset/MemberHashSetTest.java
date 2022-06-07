3be80c1072787d141c5d7d852a706b1d08d3c3df
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2022-06-07 11:15:36 +0900

    MemberHashSetTest

---
+++ Chapter 12/collection/Java/hashset/MemberHashSetTest.java
@@ -0,0 +1,29 @@
+package collection.hashset;
+
+import collection.Member;
+
+public class MemberHashSetTest {
+
+	public static void main(String[] args) {
+		
+		MemberHashSet memberHashSet = new MemberHashSet();
+		
+		Member member1 = new Member(101, "ÀÌ¼ø½Å");
+		Member member2 = new Member(102, "±èÀ¯½Å");
+		Member member3 = new Member(103, "½Å»çÀÓ´ç");
+		
+		memberHashSet.addMember(member1);
+		memberHashSet.addMember(member2);
+		memberHashSet.addMember(member3);
+		
+		memberHashSet.showAll();
+		
+		Member member4 = new Member(101, "ÀÌ¸ù·æ");
+		
+		memberHashSet.addMember(member4);
+		
+		memberHashSet.showAll();
+				
+	}
+
+}
